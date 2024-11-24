8b0396abcdfcf2a3f4565221c966fdb5a4ca3162
rajat0911q <rajat0911q@gmail.com>
2024-11-24 13:54:00 +0000

    Updated code for Form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template ,request, redirect, url_for, flash , Flask
 
 main = Blueprint('main', __name__)
+
+main.secret_key = 'rajat0911q'
 
 @main.route('/')
 def home():
     return render_template("index.html")
+
+# Add this to handle form submissions
+@main.route('/submit_form', methods=['POST'])
+def submit_form():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+    link = request.form.get('linkedin')
+    phone = request.form.get('phone')
+
+    # Debugging to see received data
+    print(f"Name: {name}, Email: {email}, Message: {message}, Link: {link}, Phone: {phone}")
+
+    flash("Thank you for reaching out! We'll get back to you soon.")
+    return redirect(url_for('index'))
